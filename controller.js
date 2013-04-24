41d26c507a28f3a9f2934cdf3c6219825e1c94d8
adam terlson <adam.terlson@gmail.com>
2013-04-23 23:08:08 -0500

    Controller updates

--- controller.js
+++ controller.js
@@ -1,61 +1,70 @@
 var Backbone = require('backbone'),
-    extend = require('extend');
+	extend = require('extend'),
+	__ = require('underscore');
 
 // Controller
-function Controller (databaseCollection, overrides) {
-    var self = this;
+function Controller (database, overrides) {
+	var self = this;
 
-    // Controller Wrapper can be made smarter so that req isn't sent, but instead params when appropriate
-    // or the req.body on POST or both on PUT
-    function controllerWrapper (fn) {
-        return function (req, res) {
-            try {
-                console.log(fn());
-                //res.send(fn(req));
-            }
-            catch (ex) {
-                // set response code to ex.type
-                // set response body to ex.body
-                // res.send.... something?
-            }
-        };
-    }
+	// Controller Wrapper can be made smarter so that req isn't sent, but instead params when appropriate
+	// or the req.body on POST or both on PUT
+	function controllerWrapper (fn) {
+		return function (req, res) {
+			try {
+				res.send(fn(req));
+			}
+			catch (ex) {
+				// set response code to ex.type
+				// set response body to ex.body
+				// res.send.... something?
+			}
+		};
+	}
 
-    self.database = databaseCollection || new Backbone.Collection();
-    
-    extend(self, {
-        list: function (req) { 
-            return this.database.toJSON();
-        },
-        
-        get: function (req) {
-            return this.database.get(req.params.id).toJSON();
-        },
-        
-        post: function (req) {
-            return this.database.set(JSON.parse(req.body)).toJSON();
-        },
-        
-        put: function (req) {
-            return this.database.set(JSON.parse(req.body)).toJSON();
-        },
-        
-        del: function (req) {
-            return this.database.remove(database.get(req.params.id));
-        },
+	database || (database = new Backbone.Collection());
+	
+	extend(self, {
+		list: function (req) { 
+			console.log('list');
+			return database.toJSON();
+		},
+		
+		get: function (req) {
+			console.log('get');
+			return database.get(req.params.id).toJSON();
+		},
+		
+		post: function (req) {
+			console.log('post');
+			var model = new Backbone.Model(req.body);
+			model.set(model.idAttribute, __.uniqueId());
+			database.add(model);
+			return model.toJSON();
+		},
+		
+		put: function (req) {
+			console.log('put');
+			return database.get(req.params.id).set(req.body).toJSON();
+		},
+		
+		delete: function (req) {
+			console.log('delete')
+			database.remove(database.get(req.params.id));
+			return;
+		},
 
-        restify: function (app, endpoint) {            
-            app.get(endpoint, controllerWrapper(self.list));
-            app.get(endpoint + '/:id', controllerWrapper(self.get));
-            app.post(endpoint, controllerWrapper(self.post));
-            app.put(endpoint + '/:id', controllerWrapper(self.put));
-            app.delete(endpoint + '/:id', controllerWrapper(self.del));
+		restify: function (app, endpoint) {            
+			app.get(endpoint, controllerWrapper(self.list));
+			app.get(endpoint + '/:id', controllerWrapper(self.get));
+			app.post(endpoint, controllerWrapper(self.post));
+			app.put(endpoint + '/:id', controllerWrapper(self.put));
+			app.delete(endpoint + '/:id', controllerWrapper(self.delete));
 
-            return self;
-        }
-    }, overrides);
+			return self;
+		}
+	}, overrides);
 
-    return self;
+	return self;
 }
 
 module.exports = Controller;
