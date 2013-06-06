bdd66efff94bef07c0836756b3d7b8b95ee69cdd
adamterlson <adam.terlson@gmail.com>
2013-06-06 17:05:50 -0500

    Updating server.js with descriptions

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.configure(function () {
 app.use('/', express.static('public'));
 
 var data = new Backbone.Collection([]);
-var controller = new Controller(data).Bind(app, 'data');
+var controller = new Controller(data).bind(app, 'data');
 
 // 99% case: basic functionality of returning data and standard error scenarios by convention
 
 var data = new Backbone.Collection([
 	{
 		id: 1,
-		description: 'The first example task.',
+		description: 'First example task.',
 		complete: true
 	},
 	{
@@ -28,15 +28,14 @@ var data = new Backbone.Collection([
 		complete: false
 	}
 ]);
-var controller = new Controller(data).Bind(app, '/api/task');
-
+var controller = new Controller(data).bind(app, '/api/task');
 // 1% case: customization of endpoint
 
 var controller404 = new Controller(data, {
 	post: function (req, res) {
 		throw { code: 404, body: { error: true }};
 	}
-}).Bind(app, '/api/will404');
+}).bind(app, '/api/will404');
  
 app.listen(8000);
 console.log('Listening on port 8000...');
